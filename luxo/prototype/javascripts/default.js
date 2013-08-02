11ea2c21166d071ca4ab95b8d03107ab3ec613c6
rocketspops <rocketspops@gmail.com>
2013-08-02 15:45:47 -0500

    adds proto for ad serv mgmt

--- luxo/prototype/javascripts/default.js
+++ luxo/prototype/javascripts/default.js
@@ -1,5 +1,37 @@
 $(function() {
 
+	$('.tooltip--notes').click(function() {
+		$('.tooltip--img').toggle();
+	});
+	
+	$('.tooltip--notes').blur(function() {
+		$('.tooltip--img').hide();
+	});
+	
+	$('.tooltip--edit').click(function() {
+		$('.overlay').show();
+	});
+	
+	$('.overlay__close').click(function() {
+		$('.overlay').hide();
+	});
+	
+	$(document).bind('keydown', function(e) {
+		if (e.which == 27) {
+		  $('.overlay').hide();
+		}
+	});
+	
+	$('.overlay__row-wrapper').on("click", "li a", function() {
+		$(this).closest('li').remove();
+	});
+	
+	$('.ad-server-button').click(function() {
+		$(".overlay__row-wrapper ul").append('<li class="blank_row"><img src="images/ad_server_mgmt/Blank_Row.png" alt="" /><a href="#"></a></li>');
+		
+	});
+	
+
 
 });
 
