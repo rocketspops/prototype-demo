d0f86fe2dcfdb39a771cdda7bf8f4633d24c43db
rocketspops <rocketspops@gmail.com>
2013-08-06 16:03:24 -0500

    change paths

---
+++ _site/static/assets/Pictos/Pictos_3/Pictos 3/READ_ME.txt
@@ -0,0 +1,7 @@
+Drew Wilson - DrewWilson.com
+
+Please read the license to make sure you abide by it.
+
+You can open the ".eps" file in Adobe Illustrator or in any similar vector editing program.
+
+The ".csh" file is a Photoshop Shapes file. You can import it into Photoshop from the Shapes Palette.
---
+++ _site/static/planner/prototype/javascripts/default.js
@@ -0,0 +1,69 @@
+$(function() {
+  
+  $('.alerts-trigger').click(function() {
+    $('.header-alerts').toggle();
+    $(this).toggleClass('clicked');
+    return false
+  });
+
+  $('.messages-container').not('.linked').resizable({
+    resize: function( event, ui ) { 
+      var topOffset = $('.top').outerHeight();
+      $('.table-container').css('top', topOffset + 'px');
+      console.log("resizing!"); 
+    } 
+  });
+
+  $('[class^="table-trigger"], [class^="tooltip-trigger"]').click(function() {
+    $(this).parent().siblings().find('a').removeClass('clicked');
+    $(this).parent().siblings().find('.js-toggle').hide();
+    $(this).siblings('a').removeClass('clicked');
+    $(this).next('img').toggle({ 
+      duration: 0,
+      complete: function () {
+        $(this).siblings('.js-toggle').hide();
+        console.log($(this).prev());
+        $(this).prev().toggleClass('clicked');
+      }
+    });
+    return false
+  });
+
+  $('.messages-trigger').click(function() {
+
+  toggleMessages(); 
+
+  $(this).toggleClass('clicked');
+    return false
+  });
+
+  $('.messages').localScroll({
+    target: '.table-container div' 
+  });
+
+  $('a.view-fullscreen').click(function(){
+    window.open('messages.html', 'Messages', 'width=1220, height=800');
+    toggleMessages();
+    $('.messages-trigger').removeClass('clicked');
+    return false;
+  });
+
+  $('a.scrollTo').click(function(){
+    console.log('scrollTo has been clicked');
+  });
+
+   $('a.popup-ctrl').click(function(){
+      $("div.table", opener.document).scrollTo('#table-03', 400);
+      return false;
+   }); 
+
+});
+
+var toggleMessages = function () {
+  $('.messages-container').toggle( 0, function() {
+    var topOffset = $('.top').outerHeight();
+    $('.table-container').css('top', topOffset + 'px');
+    console.log(topOffset);
+  });
+}
+
