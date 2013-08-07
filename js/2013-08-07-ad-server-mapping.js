35cc48eb079ec9c51443f3f8ae44882150463b46
Dave Castleton <dave.castleton@centro.net>
2013-08-07 16:24:06 -0500

    added thumbnail, selecting a source

--- js/2013-08-07-ad-server-mapping.js
+++ js/2013-08-07-ad-server-mapping.js
@@ -1,37 +1,17 @@
 $(function() {
 
-  $('.tooltip--notes').click(function() {
-    $('.tooltip--img').toggle();
-    return false;
-  });
-
-  $('.tooltip--notes').blur(function() {
-    $('.tooltip--img').hide();
-  });
-
   $('.header--trigger').click(function() {
     $('.overlay').show();
     return false;
   });
 
+	$('.overlay__select').click(function() {
+	  $('.overlay__source-wrapper').show();
+	  return false;
+	});
+
   $('.overlay__close').click(function() {
     $('.overlay').hide();
-    return false;
-  });
-
-  $(document).bind('keydown', function(e) {
-    if (e.which == 27) {
-      $('.overlay').hide();
-    }
-  });
-
-  $('.overlay__row-wrapper').on("click", "li a", function() {
-    $(this).closest('li').remove();
-    return false;
-  });
-
-  $('.ad-server-button').click(function() {
-    $(".overlay__row-wrapper ul").append('<li class="ui-front"><img src="images/ad_server_mgmt/Blank_Row.png" alt="" /><input class="ad_server_select" type="text" /><a href="#"></a></li>');
     return false;
   });
 
