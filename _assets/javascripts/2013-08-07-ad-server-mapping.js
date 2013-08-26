f955beb62d6516bcf4b94c424564c420abed413d
rocketspops <rocketspops@gmail.com>
2013-08-26 10:10:55 -0500

    add deliberate delete

--- _assets/javascripts/2013-08-07-ad-server-mapping.js
+++ _assets/javascripts/2013-08-07-ad-server-mapping.js
@@ -5,36 +5,14 @@ $(function() {
     return false;
   });
 
-	$('.overlay__select, .remove-undo').click(function() {
-	  $('.source-facebook, .icon-link').show();
-	  return false;
-	});
+  $('.overlay__select, .remove-undo').click(function() {
+    $('.source-facebook, .icon-link').show();
+    return false;
+  });
 
-	$('.source-facebook').hover(function() {
-	  $('.source-remove').show();
-	  return false;},function(){
-	  $('.source-remove').hide();
-	    return false;
-	  });
-	
-	
-	$('.source-remove').click(function() {
-	  $('.source-facebook, .icon-link').hide();
-	  return false;
-	});
-	
-	$('.source-remove').click(function() {
-	  $('.remove-confirm').show();
-	  return false;
-	});
-	
-	$('.remove-undo').click(function() {
-	  $('.source-facebook, .icon-link').show();
-	  return false;},function(){
-	  $('.remove-confirm').hide();
-	    return false;
-	});
-	
+  $('.source-remove').click(function() {
+    return false;
+  });
 
   $('.overlay__close').click(function() {
     $('.overlay').hide();
