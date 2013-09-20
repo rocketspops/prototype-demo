4cf804f464f51bb5f8d3f70a30b2319538a82fa2
rocketspops <rocketspops@gmail.com>
2013-09-20 07:02:15 -0500

    adds body to suite nav proto

---
+++ _assets/javascripts/2013-09-19-basic-suite-nav.js
@@ -0,0 +1,37 @@
+$(function() {
+  $('.selected').click(function(){
+    $(this).next('li').toggleClass('open');
+    $(this).parent().toggleClass('open');
+  });
+
+  $('.selectable li').click(function(){
+    var newSelectedText = $(this).text();
+
+    $('.contact-menu').toggleClass('open');
+
+    $('.selectable li').removeClass('active');
+    $(this).addClass('active');
+
+    $(this).parents('li').toggleClass('open');
+
+    $('.context-menu ul').removeClass('visible');
+    $('ul#' + newSelectedText.toLowerCase() + '').addClass('visible');
+
+    $('.header__nav__controls img').removeClass('visible');
+    $('img.' + newSelectedText.toLowerCase() + '').addClass('visible');
+
+    if (newSelectedText == "Operations") {
+        $('li.changeable').text('ACURALOUISVILLE005').addClass('id');
+      } else {
+        $('li.changeable').text('$100,000 budget').removeClass('id');
+      }
+
+    setTimeout(function() {
+      $('.selected span').text(newSelectedText);
+
+      
+    }, 60);
+
+  });
+
+});
