06b9ec9f3af8b5aff1a9976348ef37819942d867
James Wenzel <jcw@jcwenzeldesign.com>
2013-10-11 12:02:46 -0500

    updates to create campaign which adds the datepicker and adds a new flight date

--- _assets/javascripts/2013-10-10-campaign-create.js
+++ _assets/javascripts/2013-10-10-campaign-create.js
@@ -16,4 +16,38 @@ $(function() {
     $('.new-campaign-button').removeClass('s-is-hidden');
     return false;
   });
+
+  $('.flight-dates').on('focus', '.form__datepicker', function(event) {
+    var flightDatesOffset = $(this).parents('li').offset();
+    var DatepickerTopPosition = flightDatesOffset.top - 65;
+    var classAssignment = $(this).attr('placeholder').toLowerCase();
+    console.log(classAssignment);
+    $('#datepicker').addClass(classAssignment).css('top', DatepickerTopPosition);
+    $(this).parent().children('.i-calendar').css('background-position', '-15px 0');
+
+  });
+
+  $('.flight-dates').on('blur', '.form__datepicker', function(event) {
+    $('#datepicker').removeClass();
+    $('.i-calendar').css('background-position', '0 0');
+  });
+
+  $(window).resize(function() {
+    var flightDatesOffset = $('.flight-date').offset();
+    var DatepickerTopPosition = flightDatesOffset.top - 65;
+    $('#datepicker').css('top', DatepickerTopPosition);
+  });
+
+  $('ul.flight-date').on("click", "li a", function() {
+    $(this).closest('li').remove();
+    return false;
+  });
+
+  $('#add-flight-date').click(function() {
+    $('ul.flight-date').append('<li><div class="date-range"><span class="i-calendar"></span><input class="form__input form__datepicker" placeholder="Start" id="js-input" type="text" /></div>
+      <span class="range">-</span>
+      <div class="date-range"><span class="i-calendar"></span><input class="form__input form__datepicker" placeholder="End" id="js-input" type="text" /></div><a href="#" class="close"></a></li>');
+    return false;
+  });
+
 });
